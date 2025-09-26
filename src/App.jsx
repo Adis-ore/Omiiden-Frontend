43ddfed618c1893b6179db0136017cba41d18ff9
Adigun Oreoluwa <adisoreoluwa@gmail.com>
2025-09-26 05:48:50 +0100

    Pages correction P not p

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Home from './pages/Home'
+import Home from './Pages/Home'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
